8bd2a3a0040e54331c5ebe3d3313a0f1f05f9536
Larissa Nunes <22102365@dsn127m1242315.cfp127aluno.local>
2022-10-25 11:26:30 -0300

    criacao de tabelas

--- script_video_locadora.sql
+++ script_video_locadora.sql
@@ -28,35 +28,35 @@ describe tbl_classificacao;
 
 # tabela de generos -- cria a PK no inicio
 create table tbl_genero (
-id INT not null auto_increment primary key,
-nome varchar(45) not null, 
-unique index(id)
+	id INT not null auto_increment primary key,
+	nome varchar(45) not null, 
+	unique index(id)
 );
 
 
 # tabela de classificacao -- cria a PK no final
 create table tbl_classificacao (
-id INT not null auto_increment,
-nome varchar(20) not null,
-classificacao varchar(80) not null,
-caracteristica varchar(50) not null,
-unique index(id),
-primary key (id)	
+	id INT not null auto_increment,
+	nome varchar(20) not null,
+	classificacao varchar(80) not null,
+	caracteristica varchar(50) not null,
+	unique index(id),
+	primary key (id)	
 );
 
 # tabela de nacionalidade
 create table tbl_nacionalidade (
-id INT not null auto_increment primary key, 
-nome varchar(30) not null,
-unique index(id)
+	id INT not null auto_increment primary key, 
+	nome varchar(30) not null,
+	unique index(id)
 );
 
 # tabela de sexo 
 create table tbl_sexo(
-id INT not null auto_increment primary key,
-sigla varchar(6) not null,
-descricao varchar(30) not null, 
-unique index(id)
+	id INT not null auto_increment primary key,
+	sigla varchar(6) not null,
+	descricao varchar(30) not null, 
+	unique index(id)
 );
 
 
@@ -80,10 +80,94 @@ alter table tbl_sexo
 	# change renomeia e modifica a estrutura    
     change descricao			 descricao varchar(30) not null;
 		# coluna original		# coluna nova
-
-    
-    
         
+
+create table tbl_nacionalidade(
+	id INT not null auto_increment primary key,
+    nome varchar (30),
+    unique index(id)
+);
+
+create table tbl_filme (
+	id INT not null auto_increment primary key,
+	nome varchar(50) not null,
+	nome_original varchar(50),
+	duracao time,
+	sinopse text,
+	data_lancamento date not null, 
+	data_relancamento date,
+	foto_capa varchar (200),
+    id_classificacao int not null, 
+    constraint FK_classificacao_filme # apenas um nome (apelido) para a FK
+    foreign key (id_classificacao) # indica quem sera a chave estrangeira nessa tabela
+    references tbl_classificacao (id), # de onde virá a PK e o id dela
+    unique index(id)
+);        
+
+# permite alterar o nome da tabela
+rename table tbl_filme to tbl_filme2;
+alter table tbl_filme rename tbl_filme2;
+
+# quebrar o relacionamento (constraint) de tabelas para poder alterar nomes ou dados de tabelas que fornecem chave extrangeira
+alter table tbl_filme
+	drop foreign key FK_classificacao_filme;
+    
+alter table tbl_filme
+		add constraint FK_classificacao_filme
+			foreign key (id_classificacao)
+				references tbl_classificacao(id);
+
+
+create table tbl_ator_diretor(
+	id INT not null auto_increment primary key,
+    nome varchar (80) not null,
+    nome_artistico varchar (45),
+    data_nascimento DATE not null,
+    data_falecimento DATE, 
+    foto varchar(150),
+    id_sexo INT not null,
+    constraint FK_sexo_ator_diretor
+    foreign key (id_sexo)
+    references tbl_sexo(id),
+    unique index(id)
+);
+
+
+create table tbl_ator(
+	id INT not null auto_increment primary key,
+    quantidade_filmes INT,
+    biografia TEXT, 
+    ano_inicio_carreira YEAR,
+    id_ator_diretor INT not null,
+    constraint FK_ator_diretor_ator
+    foreign key (id_ator_diretor)
+    references tbl_ator_diretor(id),
+    unique index(id)
+);   
+
+create table tbl_diretor(
+	id INT not null auto_increment primary key,
+	quantidade_filmes INT,
+    ano_inicio_carreira year,
+    id_ator_diretor INT not null,
+    constraint FK_ator_diretor_diretor
+    foreign key (id_ator_diretor)
+    references tbl_ator_diretor(id),
+    unique index(id)
+); 	
+
+create table tbl_genero_filme(
+	id INT not null auto_increment primary key,
+    id_filme INT not null,
+    constraint FK_filme_genero_filme
+    foreign key (id_filme)	
+    references tbl_filme(id),
+    id_genero INT not null,
+    constraint FK_genero_genero_filme
+    foreign key (id_genero)
+    references tbl_genero(id),
+    unique index(id)
+);
 
 
 
