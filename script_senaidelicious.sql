3edfc2907deb725e3049aa32bddec2bd196b5178
Larissa Nunes <22102365@dsn127m1242315.cfp127aluno.local>
2022-11-17 11:30:15 -0300

    script do banco

--- script_senaidelicious.sql
+++ script_senaidelicious.sql
@@ -5,38 +5,61 @@ create database db_senailicious;
 use db_senailicious;
 
 
-create table tbl_desconto (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    porcentagem INT NOT NULL,
-    unique index (id)
-);
+##############################################################
 
-create table tbl_pizza (
+# tabelas de PRODUTOS 
+create table tbl_produto (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR (45) NOT NULL,
 	descricao TEXT NOT NULL,
     tamanho VARCHAR(45),
     tipo VARCHAR(45) NOT NULL,
     preco FLOAT NOT NULL,
-    favorita BOOLEAN NOT NULL,
-    id_desconto INT NOT NULL,
-    constraint FK_desconto_pizza
-    foreign key (id_desconto) 
-    references tbl_desconto (id),
+    desconto INT,
     unique index (id)
 );
 
 create table tbl_bebida (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR (45) NOT NULL,
-	descricao TEXT,
-    tamanho VARCHAR(45),
     tipo VARCHAR(45) NOT NULL,
-    preco FLOAT NOT NULL,
-    id_desconto INT NOT NULL,
-   constraint FK_desconto_bebida
-   foreign key (id_desconto)
-   references tbl_desconto (id),
+    id_produto INT NOT NULL,
+   constraint FK_produto_bebida
+   foreign key (id_produto)
+   references tbl_produto (id),
    unique index (id)
 );
 
+create table tbl_pizza (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	tipo VARCHAR(45) NOT NULL,
+    quantidade_vezes_favorito INT NOT NULL,
+    id_produto INT NOT NULL,
+    constraint FK_produto_pizza
+    foreign key (id_produto)
+    references tbl_produto(id),
+    unique index(id)
+);
+
+##############################################################
+
+# tabela de CLIENTES
+
+create table tbl_cliente (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    email VARCHAR (256) NOT NULL,
+    unique index(id)
+);
+
+create table tbl_celular (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ddd VARCHAR(6) NOT NULL,
+    numero VARCHAR(20) NOT NULL,
+    id_cliente INT NOT NULL,
+    constraint FK_cliente_celular
+    foreign key (id_cliente)
+    references tbl_cliente(id),
+    unique index(id)
+);
+
+create table tbl_telefone
